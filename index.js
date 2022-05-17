394c2ee35ec2a0be4af2218688c00f62cebcaf92
justin.hobbs <justinhobbs@fastmail.com>
2022-05-17 11:42:11 -0500

    commit tree 1d4fdd4be77b608f5ad77c8f9705cf9b669d8f01

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+
+//Blocks
+function distanceFromHqInBlocks(street) {
+    if (street > 42) {
+        return (street - 42);
+    } else {
+        return (42 - street);
+    }
+}
+
+//Feet
+function distanceFromHqInFeet(street) {
+    if (street > 42) {
+        return ((street - 42) * 264);
+    } else {
+        return ((42 - street) * 264);
+    }
+}
+
+//Traveled in Feet
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return ((start - destination) * 264);
+    } else {
+        return ((destination - start) * 264);
+    }
+}
+
+//Fares
+function calculatesFarePrice(start, destination) {
+     const distanceInFeet = ((Math.abs(start - destination)) * 264);
+     
+     if (distanceInFeet < 400) {
+        return 0;
+     } else if (distanceInFeet < 2000) {
+        return ((distanceInFeet - 400) * .02);
+     } else if (distanceInFeet < 2500) {
+        return 25;
+     } else {
+        return 'cannot travel that far';
+     }
+    }
